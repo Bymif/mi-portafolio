53fec75e4d27fa362f4d2d2951c05e14af63624b
miguel <miguel254@yahoo.es>
2025-02-14 12:12:10 +0100

    Actu porta

--- acciones_javascript.js
+++ acciones_javascript.js
@@ -1,1 +1,6 @@
 console.log("Bienvenido al portafolio de [Tu Nombre y Primer Apellido]");
+document.querySelectorAll('a').forEach(link => {
+    link.addEventListener('click', function() {
+        alert('Has seleccionado un proyecto.');
+    });
+});
